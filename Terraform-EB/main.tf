4b47df42717690b9261f308f2c151d85136627b6
n9br <andre.neugebauer@gmail.com>
2023-08-31 15:16:49 +0200

    prototype lambda function

--- Terraform-EB/main.tf
+++ Terraform-EB/main.tf
@@ -13,10 +13,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_lambda_function" "myLambda" {
-  filename = "myLambda.zip"
-  function_name = "0706-Users-Get"
-  role = "arn:aws:iam::646621414411:role/service-role/0706-DB-Interaction"
+resource "aws_lambda_function" "POST-city" {
+  filename = "lambda-zip/post-city.zip"
+  function_name = "POST-city"
+  # role = "arn:aws:iam::646621414411:role/service-role/0706-DB-Interaction"
   handler = "lambda_function.lambda_handler"
   runtime = "python3.10"
 }
