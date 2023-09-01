1a6042a8650d7abf94417144b0d28c3bf2b9b51e
n9br <andre.neugebauer@gmail.com>
2023-09-01 14:21:51 +0200

    exclude Terraform-EB/variables.tf

--- Terraform-EB/variables.tf
+++ Terraform-EB/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_account_id" {
   description = "AWS Account ID"
   type        = string
-  default     = "093823058718"
+  default     = "646621414411"
 }
 
 variable "aws_region" {
@@ -11,8 +11,4 @@ variable "aws_region" {
 }
 
 # Database Table Name
-<<<<<<< HEAD
 # european-cities
-=======
-# european-cities
->>>>>>> c5d18c685d398803e2df38e7cb5a258a5a8f0338
