025807bb0e33e78dcc4f7a588242c2adf06a9358
n9br <andre.neugebauer@gmail.com>
2023-09-15 17:24:19 +0200

    add play folder S3-AN

---
+++ Terraform-play/S3-AN/main.tf
@@ -0,0 +1,45 @@
+# .deploy/terraform/static-site/s3.tf
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+    bucket = aws_s3_bucket.website_bucket.id
+#   bucket = aws_s3_bucket.my-static-website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# S3 bucket ACL access
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_bucket,
+    aws_s3_bucket_public_access_block.website_bucket,
+  ]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+}
